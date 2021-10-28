a26524c059e0532ea74ee558f4f9ca05fe07ef12
arctumn <pedro.lopes@latitudde.com>
2021-10-28 09:34:07 +0100

    PEPINO

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,9 @@ function App() {
         <p>
           Bora lá
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>
+         PePino DouRado
+       </p>
       </header>
     </div>
   );
