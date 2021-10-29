0c14464be465fbfcefab88a05253e9f407ffee75
David Pires <manassa97@hotmail.com>
2021-10-29 11:58:59 +0100

    update

--- src/components/LayoutPublic.tsx
+++ src/components/LayoutPublic.tsx
@@ -24,6 +24,7 @@ import { setConstantValue } from 'typescript';
 import { ProductPage } from './ItemLayout';
 import { example_product } from '../example_data/example_Data';
 import React from 'react';
+import CatalogLayout from './CatalogLayout';
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -48,23 +49,16 @@ class LayoutPublic extends React.Component<Props, State> {
         };
     };
 
-    
-
-
     render() {
-
-        
-
 
         let content;
         if (this.state.page == "homepage") {
             content = <PublicHomepage />;
         } else if (this.state.page == "showProduct") {
-            content = <ProductPage images={example_product.images} sizes={example_product.sizes} />
+            content = <CatalogLayout />
         }
 
         return (
-
 
             <Layout>
                 <Header className="header">
@@ -79,7 +73,9 @@ class LayoutPublic extends React.Component<Props, State> {
                         <Menu.Item key="2" onClick={() => this.setState({
                             page: "showProduct"
                         })}>Homem</Menu.Item>
-                        <Menu.Item key="3">Mulher</Menu.Item>
+                        <Menu.Item key="3" onClick={() => this.setState({
+                            page: "showProduct"
+                        })}>Mulher</Menu.Item>
                     </Menu>
                     <Menu theme="dark" mode="horizontal" style={{ position: 'absolute', top: 0, right: 0 }}>
                         <Menu.Item key="4">
@@ -96,43 +92,8 @@ class LayoutPublic extends React.Component<Props, State> {
                     </Menu>
                 </Header>
 
-                <Content style={{ padding: '0 50px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-                        <Sider className="site-layout-background" width={200}>
-                            <Menu
-                                mode="inline"
-                                defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
-                                style={{ height: '100%' }}
-                            >
-                                <SubMenu key="sub1" icon={<UserOutlined />} title="Coleção">
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
-                                </SubMenu>
-                                <Menu.Item key="sub2" icon={<LaptopOutlined />}>Sapatos
-                                </Menu.Item>
-                                <SubMenu key="sub3" icon={<NotificationOutlined />} title="Acessórios">
-                                    <Menu.Item key="9">option9</Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
-                                </SubMenu>
-                            </Menu>
-                        </Sider>
-                        <Content style={{ padding: '0 24px', minHeight: 280 }}>
-
-                            {content}
-
-                        </Content>
-                    </Layout>
-                </Content>
+                {content}
+       
                 <Footer style={{ textAlign: 'center' }}> Loja Miguel 2021 </Footer>
             </Layout>
         )
