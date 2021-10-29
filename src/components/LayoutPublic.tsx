734b9bd3d3905df607bcf2f61d2320b96bf560b5
David Pires <manassa97@hotmail.com>
2021-10-29 10:11:26 +0100

    Update LayoutPublic.tsx

--- src/components/LayoutPublic.tsx
+++ src/components/LayoutPublic.tsx
@@ -23,76 +23,121 @@ import PublicHomepage from './PublicHomepage';
 import { setConstantValue } from 'typescript';
 import { ProductPage } from './ItemLayout';
 import { example_product } from '../example_data/example_Data';
+import React from 'react';
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
 const rightStyle = { position: 'absolute', top: 0, right: 0 };
 
-function LayoutPublic() {
-    return (
 
-        <Layout>
-            <Header className="header">
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        LOJA MIGUEL
-                    </Menu.Item>
-                    <Menu.Item key="2">Homem</Menu.Item>
-                    <Menu.Item key="3">Mulher</Menu.Item>
-                </Menu>
-                <Menu theme="dark" mode="horizontal" style={{ position: 'absolute', top: 0, right: 0 }}>
-                    <Menu.Item key="4">
-                        <SearchOutlined style={{ fontSize: '20px', color: 'white' }} />
-                    </Menu.Item>
-                    <Menu.Item key="5">
-                        <Badge count={20} style={{ backgroundColor: '#2db7f5'}}>
-                            <ShoppingCartOutlined style={{ fontSize: '20px', color: 'white' }} />
-                        </Badge>
-                    </Menu.Item>
-                    <Menu.Item key="6">
-                        <Avatar size="large" icon={<UserOutlined />} />
-                    </Menu.Item>
-                </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-                    <Sider className="site-layout-background" width={200}>
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{ height: '100%' }}
-                        >
-                            <SubMenu key="sub1" icon={<UserOutlined />} title="Coleção">
-                                <Menu.Item key="1">option1</Menu.Item>
-                                <Menu.Item key="2">option2</Menu.Item>
-                                <Menu.Item key="3">option3</Menu.Item>
-                                <Menu.Item key="4">option4</Menu.Item>
-                            </SubMenu>
-                            <Menu.Item key="sub2" icon={<LaptopOutlined />}>Sapatos
-                            </Menu.Item>
-                            <SubMenu key="sub3" icon={<NotificationOutlined />} title="Acessórios">
-                                <Menu.Item key="9">option9</Menu.Item>
-                                <Menu.Item key="10">option10</Menu.Item>
-                                <Menu.Item key="11">option11</Menu.Item>
-                                <Menu.Item key="12">option12</Menu.Item>
-                            </SubMenu>
-                        </Menu>
-                    </Sider>
-                    <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                    <ProductPage images={example_product.images} sizes={example_product.sizes}   /> 
-                    </Content>
-                </Layout>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}> Loja Miguel 2021 </Footer>
-        </Layout>)
-};
+interface Props {
+}
+
+interface State {
+    page: string
+}
+
+class LayoutPublic extends React.Component<Props, State> {
+
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            page: "homepage"
+        };
+    };
+
+    
+
+
+    render() {
+
+        
+
+
+        let content;
+        if (this.state.page == "homepage") {
+            content = <PublicHomepage />;
+        } else if (this.state.page == "showProduct") {
+            content = <ProductPage images={example_product.images} sizes={example_product.sizes} />
+        }
+
+        return (
+
+
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1" onClick={() => this.setState({
+                            page: "homepage"
+                        })}>
+                            <img src={logo} className="App-logo" alt="logo" />
+                            LOJA MIGUEL
+                        </Menu.Item>
+                        <Menu.Item key="2" onClick={() => this.setState({
+                            page: "showProduct"
+                        })}>Homem</Menu.Item>
+                        <Menu.Item key="3">Mulher</Menu.Item>
+                    </Menu>
+                    <Menu theme="dark" mode="horizontal" style={{ position: 'absolute', top: 0, right: 0 }}>
+                        <Menu.Item key="4">
+                            <SearchOutlined style={{ fontSize: '20px', color: 'white' }} />
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            <Badge count={20} style={{ backgroundColor: '#2db7f5' }}>
+                                <ShoppingCartOutlined style={{ fontSize: '20px', color: 'white' }} />
+                            </Badge>
+                        </Menu.Item>
+                        <Menu.Item key="6">
+                            <Avatar size="large" icon={<UserOutlined />} />
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+                        <Sider className="site-layout-background" width={200}>
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{ height: '100%' }}
+                            >
+                                <SubMenu key="sub1" icon={<UserOutlined />} title="Coleção">
+                                    <Menu.Item key="1">option1</Menu.Item>
+                                    <Menu.Item key="2">option2</Menu.Item>
+                                    <Menu.Item key="3">option3</Menu.Item>
+                                    <Menu.Item key="4">option4</Menu.Item>
+                                </SubMenu>
+                                <Menu.Item key="sub2" icon={<LaptopOutlined />}>Sapatos
+                                </Menu.Item>
+                                <SubMenu key="sub3" icon={<NotificationOutlined />} title="Acessórios">
+                                    <Menu.Item key="9">option9</Menu.Item>
+                                    <Menu.Item key="10">option10</Menu.Item>
+                                    <Menu.Item key="11">option11</Menu.Item>
+                                    <Menu.Item key="12">option12</Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+
+                            {content}
+
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}> Loja Miguel 2021 </Footer>
+            </Layout>
+        )
+    };
+
+}
 
 export default LayoutPublic;
