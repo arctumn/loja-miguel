f9d69c7b270498172ef6254e44965faa08232fcc
arctumn <arctumn@github.com>
2021-10-30 21:22:18 +0100

    Terminei basicamente a interface de um produto so falta interligar com o buttao de compra

--- src/components/ItemLayout.tsx
+++ src/components/ItemLayout.tsx
@@ -1,7 +1,8 @@
-import { Divider, Layout, Radio } from 'antd';
+import { Button, Divider, Layout, Radio } from 'antd';
 import { useState } from 'react';
 import { Product } from "../shared_interfaces/sharedInterface_product";
 import { Image, Row, Col } from 'antd';
+import "./Button.css";
 const { Header, Footer, Sider, Content } = Layout;
 
 
@@ -37,28 +38,81 @@ const ImageGallery: React.FC<Image_values> = (images) => {
         </Image.PreviewGroup>
     )
 }
-
-
+const isHidden = (elm: HTMLElement | null) => {
+    if (elm === null) return false;
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return (rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
 
 export const ProductPage: React.FC<Product> = (product: Product) => {
     //console.log(<div><p>AAAA</p></div>)
+    const [visibility, setVisibility] = useState(false)
     return (
 
-        <Row>
+        <Row onWheel={() => { setVisibility(isHidden(document.getElementById('cart'))) }}>
             <Col span={18}>
                 <ImageGallery images={product.images} />
             </Col>
             <Col span={5}>
-                <h1 style={{textAlign:"left"}}>{product.title}</h1>
-                <h3 style={{textAlign:"left"}}>{"Price: "+product.price}</h3>
+                <h1 style={{ textAlign: "left" }}>{product.title}</h1>
+                <h3 style={{ textAlign: "left" }}>{"Price: " + product.price}</h3>
                 <Divider />
-                <h3 style={{textAlign:"left"}}>{product.description}</h3>
+                <h3 style={{ textAlign: "left" }}>{product.description}</h3>
                 <Divider />
                 {SizesButtons(product.sizes)}
                 <Divider />
-                <p style={{textAlign:"justify"}}>{product.content}</p>
-                
+                <Button id='cart'
+                    onClick={() => {alert(product.title + " Added to cart\n DAVID É PRECISO ADICIONAR AO CARRINHO SOMEHOW XD"); console.log(product)}}
+                    type="primary" >Adicionar ao Carrinho</Button>
+
+                <Divider />
+                <p style={{ textAlign: "justify" }}>{product.content}</p>
+
             </Col>
+            <Footer style=
+                {visibility ? {
+                    
+                    visibility: 'visible',
+                    borderTop: '1px solid #e8e8e8',
+                    position: 'fixed',
+                    left: 0,
+                    bottom: 0,
+                    width: '100%',
+                    textAlign: 'center',
+                    display: 'flex'
+                    } : {
+                    
+                    visibility: 'hidden',
+                    borderTop: '1px solid #e8e8e8',
+                    position: 'fixed',
+                    left: 0,
+                    bottom: 0,
+                    width: '100%',
+
+                    textAlign: 'center',
+                    display: 'flex'
+                    }
+                }>
+               <Image preview={false} style={ {height: '100%'}} width={50} src={product.images[0]}/>
+               <p style={{
+                       
+                       fontSize:'1.2em',
+                       marginTop: '2.5vh',
+                       marginLeft: '1vw'
+                    }}
+               ><strong>{product.title}</strong></p>
+               <p style={{
+                        
+                        fontSize:'1.2em',
+                        marginTop: '2.5vh',
+                        marginLeft: '2vw'
+                    }}> {product.price + "€"}</p>
+                <button className="antd-copy" onClick={() => {alert(product.title + " Added to cart\n DAVID É PRECISO ADICIONAR AO CARRINHO SOMEHOW XD"); console.log(product)}} style={{
+                        
+                        
+                    }} >Adicionar ao carrinho</button>
+            </Footer>
         </Row>
 
     )
